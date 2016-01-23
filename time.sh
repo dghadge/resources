3d915d2aea61c01ff606848e257bd59da736716f
Nicolas Triantafillou <nick@triso.me>
2016-01-23 12:43:11 +1100

    update resources

---
+++ time.sh
@@ -0,0 +1,2 @@
+export AWS_DEFAULT_REGION=us-west-2
+aws cloudwatch put-metric-data --metric-name CurrentUnixTime --namespace "CloudGuru" --value `date +%s` --timestamp `date --utc "+%FT%T.%N" | sed -r 's/[[:digit:]]{6}$/Z/'`
